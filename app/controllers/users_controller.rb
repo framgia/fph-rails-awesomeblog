8b79eacdeddc72f5959d97f820e06b647387641d
Your Name <inahsanjuan@gmail.com>
2018-09-04 18:36:17 +0800

    User sign up improvements and basic form validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(
-      name: params[:user][:name],
-      email: params[:user][:email]
-    )
+    @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       redirect_to root_url
     else
       render 'new'
     end
   end
 end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
