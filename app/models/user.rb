9aee3f2e73c7a8904fe80565e8a39420cb536ff5
Your Name <inahsanjuan@gmail.com>
2018-09-04 18:55:06 +0800

    Email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  # Filter method to transform email to small cases before saving to db
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  # REGEX = Regular expression
+  # To validate correct email structure
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                                    format: { with: EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
 end
