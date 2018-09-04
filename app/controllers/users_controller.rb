8867781650d168090588c234df1fb6d4e3e6ef17
Your Name <inahsanjuan@gmail.com>
2018-09-04 18:22:46 +0800

    User model and basic create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    user = User.new(
+      name: params[:user][:name],
+      email: params[:user][:email]
+    )
+
+    if user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
 end
