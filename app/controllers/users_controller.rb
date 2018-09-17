362dba48967bb02f35ddfd7d5adfe613c509cde9
Your Name <inahsanjuan@gmail.com>
2018-09-17 15:00:22 +0800

    Page authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login, except: [:new, :create]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
   end
@@ -20,9 +22,16 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-end
 
-private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def require_login
+      unless logged_in?
+        flash[:info] = "Please log in."
+        redirect_to login_url
+      end
+    end
+end
