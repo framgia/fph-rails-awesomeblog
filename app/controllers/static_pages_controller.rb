f8646af9a4da5213dc5f7f8fdefa78fcb44fe546
Your Name <inahsanjuan@gmail.com>
2018-09-25 15:25:41 +0800

    Micropost form and home feed layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # Different home view if there is a logged user
+    render 'users/home_feed' if logged_in?
   end
 
   def about
